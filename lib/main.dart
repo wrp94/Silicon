a57cbc4955cf6f1e6a61143646c6874eaa38e8ca
William Parker <william.parker.20@cnu.edu>
2023-02-09 15:03:47 -0500

    stopped short of adding multi-touch

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/gestures.dart';
 
 void main() => runApp(const SiliconApp());
 
@@ -73,12 +74,22 @@ class AppStateWidgetState extends State<AppStateWidget> {
   }
 }
 
-class Chooser extends StatelessWidget {
+/// `Chooser` is a `StatefulWidget` that creates a `_ChooserState` which is a
+/// `State` that builds a `Scaffold`
+class Chooser extends StatefulWidget {
   const Chooser({super.key});
 
   @override
+  State<Chooser> createState() => _ChooserState();
+}
+
+class _ChooserState extends State<Chooser> {
+
+  @override
   Widget build(BuildContext context) {
-    throw UnimplementedError();
+    return Scaffold(
+      //TODO implement multi-touch recognition
+    );
   }
 }
 
